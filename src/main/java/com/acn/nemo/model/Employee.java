3f7513453dceb6a89b077f17734681ea3244664d
Massimo Caputo <massimo.caputo@accenture.com>
2024-04-21 23:25:36 +0200

    Modify entity

--- src/main/java/com/acn/nemo/model/Employee.java
+++ src/main/java/com/acn/nemo/model/Employee.java
@@ -1,9 +1,6 @@
 package com.acn.nemo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -14,6 +11,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @NamedQueries(
         {
                 @NamedQuery(name = "Employee.findAll", query = "SELECT e from Employee e" )
