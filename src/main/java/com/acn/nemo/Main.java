f18e2c66c1831be31e629510759ee38dfc0bb00c
Massimo, Caputo <massimo.caputo@accenture.com>
2024-04-26 16:38:05 +0200

    Comment

--- src/main/java/com/acn/nemo/Main.java
+++ src/main/java/com/acn/nemo/Main.java
@@ -1,7 +1,13 @@
 package com.acn.nemo;
 
+import org.apache.logging.log4j.Logger;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
 public class Main {
+    private static final Logger log = getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       log.info("Hello JpaTutorials!");
     }
 }
